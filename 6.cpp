8cc688769dfdcc63fac0fba35d21808b8caaced6
FadelSM <rasyahstore8888@gmail.com>
2026-02-08 13:21:56 +0700

    Add parseInts function to parse integers from string
    Implement parseInts function to parse integers from a string.

--- 6.cpp
+++ 6.cpp
@@ -4,16 +4,27 @@
 using namespace std;
 
 vector<int> parseInts(string str) {
-	// Complete this function
-}
+    vector<int> result;
+    stringstream ss(str);
+    int num;
+    char comma;
+
+    while (ss >> num) {
+        result.push_back(num);
+        ss >> comma; // buang koma
+    }
+
+    return result;
+}   // INI WAJIB, penutup fungsi parseInts
 
 int main() {
     string str;
     cin >> str;
+
     vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
+    for (int i = 0; i < integers.size(); i++) {
         cout << integers[i] << "\n";
     }
-    
+
     return 0;
 }
